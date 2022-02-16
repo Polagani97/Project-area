662d5ecafe0e47f38cf621668e35ef33c34eaae6
Samhitha Makham <smakham@deakin.ed.au>
2022-02-16 12:30:47 +1100

    git commit

---
+++ public/env.js
@@ -0,0 +1,85 @@
+var username
+var password
+var user
+// send data to server
+const submitUser=(user)=>{
+    $.ajax({
+        url: '/api/users',
+        contentType: 'application/json',
+        data: JSON.stringify(user),
+        type: 'POST',
+        success: function(result) {
+            alert('User added successfully')
+        }
+    });
+}
+
+const newUser=()=>{
+    let username = $('#username').val()
+    let password = $('#password').val()
+    let email = $('#email').val()
+    let mobile = $('#mobile').val()
+    let appliances = $('#appliances').val()
+
+    let user={username,password,email,mobile,appliances}
+
+    console.log(user)
+    submitUser(user)
+}
+
+const formUser=()=>{
+    username = $('#username').val()
+    password = $('#password').val()
+
+    user={username,password}
+
+    console.log(user)
+}
+
+
+//connect to socket
+/*let socket = io();
+
+socket.on('number',(msg) => {
+    console.log('Random number: '+msg);
+    $('#socketOut').html(msg)
+})*/
+
+//appends the comment row with the objects of type comment
+/*listComments=(comments)=>{
+    comments.forEach(comment => {
+        console.log(comment)
+        let item ='<div class="card">'+
+        '<div class="card-content">'+
+        '<span class="card-title activator grey-text text-darken-4">'+comment.name+'</span>'+
+        '</div>'+
+        '<div class="card-reveal">'+
+        '<p>'+comment.description+'</p>'+
+     '</div>'+
+     '</div>'          
+    
+    $('#listComments').append(item)
+    });
+}*/
+
+// INITIALIZATION 
+/*const dummyComment={
+    name:'priyanka',
+    description:'This is a great example for personal portfolio'
+}*/
+
+//let dummyData=[dummyComment,dummyComment]
+
+/*$(document).ready(function(){
+    console.log('Ready')
+  
+    // get data and build the ui component
+    //listComments(dummyData)
+  
+    //bind the button
+    //$('#testButton').click(testButtonFunction)
+  
+    //requestComments()
+
+  })
+*/
---
+++ server.js
@@ -0,0 +1,106 @@
+let envf = require("./public/env");
+let express = require("express");
+let app = express();
+
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+
+
+//serve comments data to the requestor
+app.get('/api/users',(req,res)=>{
+    console.log('users requested')
+    //get comments from database
+    //return comments to the user
+    //res.send(dummyData)
+    getUsers(res)
+})
+
+app.post('/api/users',(req,res)=>{
+    console.log('New User Added')
+    console.log('body',req.body)
+    let user = req.body;
+    //dummyData.push(comment)
+    //res.send({result:200})
+    insertUser(user,res)
+})
+
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    });
+    setInterval(()=>{
+        socket.emit('number',parseInt(Math.random()*10));
+    },1000);
+});
+
+
+
+//Data base Connection
+const uri = "mongodb+srv://priyanka:Priy%402127@cluster0.2dgu3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+let usersCollection;
+
+//this function is used to open the connection
+const openConnection = (message)=> {
+    client.connect((err,db) => {
+      usersCollection = client.db("allUsers").collection("users");
+        if(!err){
+            console.log('Database Connected')
+        }
+        else{
+            console.log('error in if',err)
+        }
+    });
+}
+
+//insert comment into the db
+const insertUser = (user,res)=>{
+    //insert into collection
+    usersCollection.insertOne(user,(err,result)=>{
+        console.log('User Added',result)
+        res.send({result:200})
+    })
+}
+
+//retrieve all comments
+const getUsers=(res)=>{
+  usersCollection.find({}).toArray(function(err,result){
+        if(err) throw err;
+        console.log(result)
+        res.send(result)
+        checkUser(envf.user,result)
+    })
+}
+
+const checkUser=(user,result)=>{
+    body = JSON.parse(result);
+    console.log("body",body)
+    for (var i=body.length in body)
+    {
+        if(i.username==body.username && i.password == user.password)
+        {
+            alert('User Login Success')
+        }
+        else
+        {
+            alert('User Login failure')
+        }
+    }
+
+}
+
+openConnection()
+
+http.listen(port,()=>{
+    console.log("listening on port",port);
+});
